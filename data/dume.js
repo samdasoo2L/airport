c5004345e9b7626a25b0216ab1f35458480a5560
Kang MinSeok <alstjr8125@naver.com>
2025-03-25 04:27:41 +0900

    초안완성

---
+++ data/dume.js
@@ -0,0 +1,53 @@
+const dData = [
+  {
+    title: "제 77주년 4.3 희생자 추념식",
+    sub: "2025년 4월 3일 목요일 오전 10시 (제주 4.3 평화공원)",
+    pic: "data/pic/d1.jpg",
+  },
+  {
+    title: "세계 평화의 섬 제주",
+    sub: "20th",
+    pic: "data/pic/d2.jpg",
+  },
+  {
+    title: "의료계 파업 대비 문 여는 병.의원 정보 안내",
+    sub: "2024.02.23. ~ 상황 종료시까지",
+    pic: "data/pic/d3.jpg",
+  },
+  {
+    title: "제주 고향 사랑 기부제",
+    sub: "세액공제, 답례품, 관광지 할인 혜택까지!",
+    pic: "data/pic/d4.jpg",
+  },
+];
+
+const wData = [
+  {
+    title: "기온",
+    sub: "27도",
+    pic: "data/pic/w1.jpg",
+  },
+  {
+    title: "미세먼지",
+    sub: "나쁨",
+    pic: "data/pic/w2.jpg",
+  },
+];
+
+const fData = [
+  {
+    title: "제주 해녀축제",
+    sub: "2025.09.21. ~ 2025.09.22.",
+    pic: "data/pic/f1.jpg",
+  },
+  {
+    title: "귤꽃향기축제",
+    sub: "2025.04.27. ~ 2025.05.12.",
+    pic: "data/pic/f2.jpg",
+  },
+  {
+    title: "제주 도새기 축제 (맛의 비밀)",
+    sub: "2025.10.12. ~ 2025.10.13.",
+    pic: "data/pic/f3.jpg",
+  },
+];
--- main.js
+++ main.js
@@ -1,18 +1,46 @@
 const cardD = document.querySelector(".card-d");
+const cardImgD = document.querySelector(".card-img-d");
+const cardInfo1D = document.querySelector(".card-info-1-d");
+const cardInfo2D = document.querySelector(".card-info-2-d");
+
 const cardW = document.querySelector(".card-w");
+const cardImgW = document.querySelector(".card-img-w");
+const cardInfo1W = document.querySelector(".card-info-1-w");
+const cardInfo2W = document.querySelector(".card-info-2-w");
+
 const cardF = document.querySelector(".card-f");
-const cardImg = document.querySelector(".card-img");
-const cardInfo1 = document.querySelector(".card-info-1");
+const cardImgF = document.querySelector(".card-img-f");
+const cardInfo1F = document.querySelector(".card-info-1-f");
+const cardInfo2F = document.querySelector(".card-info-2-f");
+
+// let dData = [];
+let dDataL = 0;
+let dDataN = 0;
+// let wData = [];
+let wDataL = 0;
+let wDataN = 0;
+// let fData = [];
+let fDataL = 0;
+let fDataN = 0;
 
 function cardDEvent() {
+  if (dDataL == 0) {
+    dDataL = dData.length;
+  } else {
+    dDataN++;
+    if (dDataN == dDataL) {
+      dDataN = 0;
+    }
+  }
   cardD.classList.remove("come");
   cardD.classList.add("hide");
   setTimeout(() => {
     cardD.classList.remove("hide");
     cardD.classList.add("make");
     //데이터교체
-    cardImg.src = "data/c.jpg";
-    cardInfo1.innerText = "234235435";
+    cardImgD.src = dData[dDataN].pic;
+    cardInfo1D.innerText = dData[dDataN].title;
+    cardInfo2D.innerText = dData[dDataN].sub;
   }, 1000);
   setTimeout(() => {
     cardD.classList.remove("make");
@@ -21,14 +49,23 @@ function cardDEvent() {
 }
 
 function cardWEvent() {
+  if (wDataL == 0) {
+    wDataL = wData.length;
+  } else {
+    wDataN++;
+    if (wDataN == wDataL) {
+      wDataN = 0;
+    }
+  }
   cardW.classList.remove("come");
   cardW.classList.add("hide");
   setTimeout(() => {
     cardW.classList.remove("hide");
     cardW.classList.add("make");
     //데이터교체
-    cardImg.src = "data/c.jpg";
-    cardInfo1.innerText = "234235435";
+    cardImgW.src = wData[wDataN].pic;
+    cardInfo1W.innerText = wData[wDataN].title;
+    cardInfo2W.innerText = wData[wDataN].sub;
   }, 1000);
   setTimeout(() => {
     cardW.classList.remove("make");
@@ -37,14 +74,23 @@ function cardWEvent() {
 }
 
 function cardFEvent() {
+  if (fDataL == 0) {
+    fDataL = fData.length;
+  } else {
+    fDataN++;
+    if (fDataN == fDataL) {
+      fDataN = 0;
+    }
+  }
   cardF.classList.remove("come");
   cardF.classList.add("hide");
   setTimeout(() => {
     cardF.classList.remove("hide");
     cardF.classList.add("make");
     //데이터교체
-    cardImg.src = "data/c.jpg";
-    cardInfo1.innerText = "234235435";
+    cardImgF.src = fData[fDataN].pic;
+    cardInfo1F.innerText = fData[fDataN].title;
+    cardInfo2F.innerText = fData[fDataN].sub;
   }, 1000);
   setTimeout(() => {
     cardF.classList.remove("make");
@@ -54,6 +100,9 @@ function cardFEvent() {
 
 function cardFunction() {
   // 데이터 가져오는 코드 (비동기 async await)
+  // dData =
+  // wData =
+  // fData =
 
   setTimeout(() => {
     setInterval(cardDEvent, 10000);
